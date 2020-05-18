27ee27795ad962b30c0ac87630238cf5709a47ef
Bengt Marten Agren <maraingist@me.com>
2020-05-18 23:50:47 +0800

    Added serial example for STM32G0.

---
+++ examples/NUCLEO-G070RB/Serial/main.cpp
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <cstring>
+#include <usart.h>
+#include <redirect.h>
+
+using serial = usart_t<2, PA2, PA3>;
+using ld4 = output_t<PA5>;
+
+template<> void handler<interrupt::USART2>()
+{
+    ld4::toggle();
+    serial::isr();
+}
+
+int main()
+{
+    ld4::setup();
+    serial::setup<230400>();
+    //hal::nvic<interrupt::USART2>::enable();
+    stdio_t::bind<serial>();
+    interrupt::enable();
+
+    printf("Hello STM32G070!\n");
+
+    for (;;)
+    {
+        static char buf[256];
+
+        printf("> ");
+        fflush(stdout);
+        if (fgets(buf, sizeof(buf), stdin))
+        {
+            buf[strcspn(buf, "\r\n")] = 0;
+            printf("got = '%s'\n", buf);
+        }
+    }
+}
+

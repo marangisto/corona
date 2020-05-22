806d72a40cb93582c37bf04a34cc6c25f14087d2
Bengt Marten Agren <maraingist@me.com>
2020-05-22 19:58:29 +0800

    Cleaned up usart traits and usage.

--- include/usart.h
+++ include/usart.h
@@ -9,26 +9,19 @@ template<int INST> struct usart_traits {};
 template<> struct usart_traits<1>
 {
     static constexpr peripheral_enum_t peripheral = USART1;
-    static constexpr alternate_function_t tx = USART1_TX;
-    static constexpr alternate_function_t rx = USART1_RX;
+    static constexpr alternate_function_t TX = USART1_TX;
+    static constexpr alternate_function_t RX = USART1_RX;
 };
 
 template<> struct usart_traits<2>
 {
     static constexpr peripheral_enum_t peripheral = USART2;
-    static constexpr alternate_function_t tx = USART2_TX;
-    static constexpr alternate_function_t rx = USART2_RX;
+    static constexpr alternate_function_t TX = USART2_TX;
+    static constexpr alternate_function_t RX = USART2_RX;
 };
-
-template<int INST>
-using usart = peripheral_t<mcu_svd, usart_traits<INST>::peripheral>;
 
 template<int INST, gpio_pin_t TX, gpio_pin_t RX> struct usart_t
 {
-private:
-    typedef fifo_t<char, INST, 16> fifo;
-    using _ = typename usart<INST>::T;
-
 public:
     template
         < uint32_t              baud = 9600
@@ -39,12 +32,12 @@ public:
         >
     static inline void setup()
     {
-        typename usart<INST>::T& USART = usart<INST>::V;
+        typename usart::T& USART = usart::V;
 
-        alternate_t<TX, usart_traits<INST>::tx>::template setup<speed>();
-        alternate_t<RX, usart_traits<INST>::rx>::template setup<pull_up>();
+        alternate_t<TX, traits::TX>::template setup<speed>();
+        alternate_t<RX, traits::RX>::template setup<pull_up>();
 
-        clock_control_t<usart<INST>>::enable(); // enable clock
+        clock_control_t<usart>::enable(); // enable clock
         USART.BRR = sys_clock::freq() / baud;   // set baud-rate FIXME: need clock reference!
         USART.CR1 |= _::CR1_RESET_VALUE     // reset control register 1
                   | _::CR1_TE               // enable transmitter
@@ -60,7 +53,7 @@ public:
     static inline void write(uint8_t x)
     {
         while (!tx_empty());
-        usart<INST>::V.TDR = x;
+        usart::V.TDR = x;
     }
 
     static inline void write(const char *s)
@@ -80,7 +73,7 @@ public:
     __attribute__((always_inline))
     static inline void isr()
     {
-        fifo::put(usart<INST>::V.RDR);
+        fifo::put(usart::V.RDR);
     }
 
     __attribute__((always_inline))
@@ -92,13 +85,19 @@ public:
     __attribute__((always_inline))
     static inline bool tx_empty()
     {
-        return usart<INST>::V.ISR & _::ISR_TXE;
+        return usart::V.ISR & _::ISR_TXE;
     }
 
     __attribute__((always_inline))
     static inline bool rx_not_empty()
     {
-        return usart<INST>::V.ISR & _::ISR_RXNE;
+        return usart::V.ISR & _::ISR_RXNE;
     }
+
+private:
+    using traits = usart_traits<INST>;
+    using usart = peripheral_t<mcu_svd, traits::peripheral>;
+    using fifo = fifo_t<char, INST, 16>;
+    using _ = typename usart::T;
 };
 

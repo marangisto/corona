d23255d6e92e8c8f857d0f0ce14bbd83bf59d92a
Bengt Marten Agren <marangisto@me.com>
2020-05-30 15:35:44 +0800

    First multi-board example (Blink).

---
+++ examples/Blink/main.cpp
@@ -0,0 +1,16 @@
+#include <board.h>
+#include <gpio.h>
+
+using led = output_t<board::LED1>;
+
+int main()
+{
+    led::setup();
+
+    for (;;)
+    {
+        led::toggle();
+        sys_tick::delay_ms(1001);
+    }
+}
+

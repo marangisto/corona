250f7e28aa47b16f72b4772eaafc155a17c1e1fb
Bengt Marten Agren <marangisto@me.com>
2020-05-30 14:44:37 +0800

    Harmonized STK register names.

--- include/stm32/stm32f1/device/stk.h
+++ include/stm32/stm32f1/device/stk.h
@@ -14,25 +14,25 @@
 
 struct stm32f100_stk_t
 {
-    volatile uint32_t CTRL;  // [read-write] SysTick control and status register
+    volatile uint32_t CSR;   // [read-write] SysTick control and status register
     volatile uint32_t LOAD_; // [read-write] SysTick reload value register
-    volatile uint32_t VAL;   // [read-write] SysTick current value register
+    volatile uint32_t CVR;   // [read-write] SysTick current value register
     volatile uint32_t CALIB; // [read-write] SysTick calibration value register
 
-    static constexpr uint32_t CTRL_RESET_VALUE = 0x0;
-    static constexpr uint32_t CTRL_ENABLE = 0x1;        // Counter enable
-    static constexpr uint32_t CTRL_TICKINT = 0x2;       // SysTick exception request enable
-    static constexpr uint32_t CTRL_CLKSOURCE = 0x4;     // Clock source selection
-    static constexpr uint32_t CTRL_COUNTFLAG = 0x10000; // COUNTFLAG
+    static constexpr uint32_t CSR_RESET_VALUE = 0x0;
+    static constexpr uint32_t CSR_ENABLE = 0x1;        // Counter enable
+    static constexpr uint32_t CSR_TICKINT = 0x2;       // SysTick exception request enable
+    static constexpr uint32_t CSR_CLKSOURCE = 0x4;     // Clock source selection
+    static constexpr uint32_t CSR_COUNTFLAG = 0x10000; // COUNTFLAG
 
     static constexpr uint32_t LOAD__RESET_VALUE = 0x0;
     template<uint32_t X>
     static constexpr uint32_t LOAD__RELOAD =   // RELOAD value
         bit_field_t<0, 0xffffff>::value<X>();
 
-    static constexpr uint32_t VAL_RESET_VALUE = 0x0;
+    static constexpr uint32_t CVR_RESET_VALUE = 0x0;
     template<uint32_t X>
-    static constexpr uint32_t VAL_CURRENT =   // Current counter value
+    static constexpr uint32_t CVR_CURRENT =   // Current counter value
         bit_field_t<0, 0xffffff>::value<X>();
 
     static constexpr uint32_t CALIB_RESET_VALUE = 0x0;
--- include/stm32/stm32g4/device/stk.h
+++ include/stm32/stm32g4/device/stk.h
@@ -14,25 +14,25 @@
 
 struct stm32g431xx_stk_t
 {
-    volatile uint32_t CTRL;  // [read-write] SysTick control and status register
-    volatile uint32_t LOAD;  // [read-write] SysTick reload value register
-    volatile uint32_t VAL;   // [read-write] SysTick current value register
+    volatile uint32_t CSR;   // [read-write] SysTick control and status register
+    volatile uint32_t RVR;   // [read-write] SysTick reload value register
+    volatile uint32_t CVR;   // [read-write] SysTick current value register
     volatile uint32_t CALIB; // [read-write] SysTick calibration value register
 
-    static constexpr uint32_t CTRL_RESET_VALUE = 0x0;
-    static constexpr uint32_t CTRL_ENABLE = 0x1;        // Counter enable
-    static constexpr uint32_t CTRL_TICKINT = 0x2;       // SysTick exception request enable
-    static constexpr uint32_t CTRL_CLKSOURCE = 0x4;     // Clock source selection
-    static constexpr uint32_t CTRL_COUNTFLAG = 0x10000; // COUNTFLAG
+    static constexpr uint32_t CSR_RESET_VALUE = 0x0;
+    static constexpr uint32_t CSR_ENABLE = 0x1;        // Counter enable
+    static constexpr uint32_t CSR_TICKINT = 0x2;       // SysTick exception request enable
+    static constexpr uint32_t CSR_CLKSOURCE = 0x4;     // Clock source selection
+    static constexpr uint32_t CSR_COUNTFLAG = 0x10000; // COUNTFLAG
 
-    static constexpr uint32_t LOAD_RESET_VALUE = 0x0;
+    static constexpr uint32_t RVR_RESET_VALUE = 0x0;
     template<uint32_t X>
-    static constexpr uint32_t LOAD_RELOAD =   // RELOAD value
+    static constexpr uint32_t RVR_RELOAD =   // RELOAD value
         bit_field_t<0, 0xffffff>::value<X>();
 
-    static constexpr uint32_t VAL_RESET_VALUE = 0x0;
+    static constexpr uint32_t CVR_RESET_VALUE = 0x0;
     template<uint32_t X>
-    static constexpr uint32_t VAL_CURRENT =   // Current counter value
+    static constexpr uint32_t CVR_CURRENT =   // Current counter value
         bit_field_t<0, 0xffffff>::value<X>();
 
     static constexpr uint32_t CALIB_RESET_VALUE = 0x0;

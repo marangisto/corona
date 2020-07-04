8429ab9b41e8d4954bd1012b96ee5e07ec4c578e
Bengt Marten Agren <marangisto@me.com>
2020-07-04 10:13:14 +0800

    Corrected return frequency.

--- include/stm32/stm32f7/driver/sysclock.h
+++ include/stm32/stm32f7/driver/sysclock.h
@@ -75,7 +75,6 @@ static inline uint32_t clock_tree_init()
     fpu_cpacr_t::V.CPACR |= fpu_cpacr_t::T::CPACR_CP::W(0xf); // enable fpu
     __asm volatile ("dsb");         // data pipe-line reset
     __asm volatile ("isb");         // instruction pipe-line reset
-    //return 100000000;
-    return 16000000;
+    return 216000000;
 }
 

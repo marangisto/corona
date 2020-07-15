3e7609eabcb1c882dbdcbe35decc7be41c4dab39
Bengt Marten Agren <marangisto@me.com>
2020-07-15 21:50:55 +0800

    Added and verified NUCLEO-F072 board.

--- include/board.h
+++ include/board.h
@@ -112,6 +112,18 @@ struct board_traits_t<STM32F0_DISCO>
 };
 
 template<>
+struct board_traits_t<NUCLEO_F072RB>
+{
+    using led1 = output_t<PA5>;
+    using btn1 = button_t<PC13>;
+    static constexpr input_type_t btn_type = pull_up;
+    using probe = output_t<PA8>;
+    using serial = usart_t<2, PA2, PA3>;
+    static constexpr interrupt::interrupt_t serial_interrupt = interrupt::USART2;
+};
+
+/*
+template<>
 struct board_traits_t<NUCLEO_F207ZG>
 {
     using led1 = output_t<PB0>;
@@ -123,6 +135,7 @@ struct board_traits_t<NUCLEO_F207ZG>
     using serial = usart_t<3, PD8, PD9>;
     static constexpr interrupt::interrupt_t serial_interrupt = interrupt::USART3;
 };
+*/
 
 template<>
 struct board_traits_t<STM32F411E_DISCO>

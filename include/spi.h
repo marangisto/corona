4a0db0eff8ddd114db241ae596f5d59b77330163
Bengt Marten Agren <marangisto@me.com>
2020-07-26 09:48:46 +0800

    Cosmetic fixes.

--- include/spi.h
+++ include/spi.h
@@ -9,7 +9,7 @@ enum spi_mode_t
     , mode_3    // CPOL=1, CPHA=1
     };
 
-enum spi_bit_order_t { msb_first, lsb_first };
+enum spi_order_t { msb_first, lsb_first };
 
 enum spi_clock_divider_t
     { fpclk_2
@@ -22,34 +22,55 @@ enum spi_clock_divider_t
     , fpclk_256
     };
 
-template<typename _, spi_mode_t> struct spi_mode_traits {};
+template<typename _, spi_mode_t>
+struct spi_mode_traits {};
 
-template<typename _> struct spi_mode_traits<_, mode_0>
+template<typename _>
+struct spi_mode_traits<_, mode_0>
 {
     static constexpr uint32_t value = 0;
 };
 
-template<typename _> struct spi_mode_traits<_, mode_1>
+template<typename _>
+struct spi_mode_traits<_, mode_1>
 {
     static constexpr uint32_t value = _::CR1_CPHA;
 };
 
-template<typename _> struct spi_mode_traits<_, mode_2>
+template<typename _>
+struct spi_mode_traits<_, mode_2>
 {
     static constexpr uint32_t value = _::CR1_CPOL;
 };
 
-template<typename _> struct spi_mode_traits<_, mode_3>
+template<typename _>
+struct spi_mode_traits<_, mode_3>
 {
     static constexpr uint32_t value = _::CR1_CPHA | _::CR1_CPOL;
 };
 
-template<int INST, pin_t SCK, pin_t MOSI, pin_t MISO> struct spi_t
+template<typename _, spi_order_t>
+struct spi_order_traits {};
+
+template<typename _>
+struct spi_order_traits<_, msb_first>
+{
+    static constexpr uint32_t value = 0;
+};
+
+template<typename _>
+struct spi_order_traits<_, lsb_first>
+{
+    static constexpr uint32_t value = _::CR1_LSBFIRST;
+};
+
+template<int INST, pin_t SCK, pin_t MOSI, pin_t MISO>
+struct spi_t
 {
 public:
     template
         < spi_mode_t            mode    = mode_0
-        , spi_bit_order_t       order   = msb_first
+        , spi_order_t           order   = msb_first
         , spi_clock_divider_t   divider = fpclk_256
         , output_speed_t        speed   = low_speed
         >
@@ -61,18 +82,18 @@ public:
         alternate_t<MOSI, traits::MOSI>::template setup<speed>();
         alternate_t<MISO, traits::MISO>::template setup<floating>();
 
-        spi_traits<INST>::template enable<rcc_t>();   // enable clock
+        spi_traits<INST>::template enable<rcc_t>(); // enable clock
 
-        SPI.CR1 = _::CR1_RESET_VALUE            // reset control register 1
-                | _::CR1_MSTR                   // master mode
-                | _::CR1_BR::W(divider)         // clock divider
-                | spi_mode_traits<_, mode>::value   // SPI-mode
-                | (order == lsb_first ?  _::CR1_LSBFIRST : 0) // lsb first
+        SPI.CR1 = _::CR1_RESET_VALUE                // reset register
+                | _::CR1_MSTR                       // master mode
+                | _::CR1_BR::W(divider)             // clock divider
+                | spi_mode_traits<_, mode>::value   // polarity & phase
+                | spi_order_traits<_, order>::value // bit-order
                 ;
-        SPI.CR2 = _::CR2_RESET_VALUE            // reset control register 2
-                |  _::CR2_SSOE;                 // ss output enable
+        SPI.CR2 = _::CR2_RESET_VALUE                // reset register
+                |  _::CR2_SSOE;                     // enable ss output
                 ;
-        SPI.CR1 |= _::CR1_SPE;                  // enable spi
+        SPI.CR1 |= _::CR1_SPE;                      // enable spi
     }
 
     __attribute__((always_inline))
@@ -100,7 +121,8 @@ public:
     {
         while ((spi::V.SR & (_::SR_BSY | _::SR_TXE)) != _::SR_TXE);
     }
-/*
+
+    /*
     enum interrupt_t
         { err_interrupt = _::CR2_ERRIE
         , rx_interrupt = _::CR2_RXNEIE
@@ -114,9 +136,11 @@ public:
         spi::V.CR2 &= ~mask;
         spi::V.CR2 |= flags & mask;
     }
-*/
+    */
+
 private:
     using traits = spi_traits<INST>;
     using spi = typename traits::spi;
     using _ = typename spi::T;
 };
+

2c24167131b9da1f452d94cb3a97a3e8b6cc5336
Bengt Marten Agren <marangisto@me.com>
2020-08-10 15:42:50 +0800

    Added support for F0.

--- tools/ClockTree/app/Main.hs
+++ tools/ClockTree/app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards, DuplicateRecordFields #-}
 module Main (main) where
 
 import System.Console.CmdArgs
@@ -33,10 +33,57 @@ options = Main.Options
             ]
 
 maxFreq :: Family -> Double
+maxFreq STM32F2 = 120e6
 maxFreq STM32G4 = 170e6
 maxFreq x = error $ "don't know max frequency for " <> show x
 
-data ClockTreeG4 = ClockTreeG4
+main :: IO ()
+main = do
+    opts@Options{..} <- cmdArgs options
+    case family of
+        [fam] -> solve fam $ fromMaybe (maxFreq fam) freq
+        _ -> error "specify one family"
+
+class ClockTree a where
+    gen :: Double -> [a]
+
+solve :: Family -> Double -> IO ()
+solve STM32F2 freq
+  = mapM_ print
+  . filter (\F2{..} -> fPllP == freq && fPllQ == 48e6)
+  $ gen 16e6
+solve STM32G4 freq
+  = mapM_ print
+  . filter (\G4{..} -> fSYS == freq && fPllP == freq && fPllQ == freq)
+  $ gen 16e6
+
+data F2 = F2
+    { pllN      :: Double
+    , pllM      :: Double
+    , pllP      :: Double
+    , pllQ      :: Double
+    , fPllP     :: Double
+    , fPllQ     :: Double
+    , fVCO      :: Double
+    } deriving (Show)
+
+instance ClockTree F2 where
+  gen fCin =
+    [ let fVCO = fCin * pllN / pllM
+          fPllQ = fVCO / pllQ
+          fPllP = fVCO / pllP
+       in F2{..}
+    | pllN <- [64..432]
+    , pllM <- [2..63]
+    , pllP <- [2, 4, 6, 8]
+    , pllQ <- [2..15]
+    , let pllIn = fCin / pllM
+       in pllIn >= 1e6 && pllIn <= 2e6     -- required range!
+    , let fVCO = fCin * pllN / pllM
+       in fVCO >= 64e6 && fVCO <= 432e6    -- allowed range!
+    ]
+
+data G4 = G4
     { pllN      :: Double
     , pllM      :: Double
     , pllP      :: Double
@@ -49,39 +96,23 @@ data ClockTreeG4 = ClockTreeG4
     , fVCO      :: Double
     } deriving (Show)
 
-main :: IO ()
-main = do
-    opts@Options{..} <- cmdArgs options
-    case family of
-        [fam] -> solve fam $ fromMaybe (maxFreq fam) freq
-        _ -> error "specify one family"
-
-solve :: Family -> Double -> IO ()
-solve STM32G4 freq = do
-    let xs = id
-           . filter ((==freq) . fPllQ)
-           . filter ((==freq) . fPllP)
-           . filter ((==freq) . fSYS)  -- exactly 64MHz
-           $ genClocks 16e6 -- hsi clock
-    mapM_ print xs
-
-genClocks :: Double -> [ClockTreeG4]
-genClocks hsi =
-    [ let fVCO = hsi * pllN / pllM
+instance ClockTree G4 where
+  gen fCin =
+    [ let fVCO = fCin * pllN / pllM
           fSYS = fVCO / pllR
           fPllQ = fVCO / pllQ
           fPllP | pllPDIV == 0 = fVCO / pllP
                 | otherwise = fVCO / pllPDIV
-       in ClockTreeG4{..}
+       in G4{..}
     | pllN <- [8..127]
     , pllM <- [1..16]
     , pllP <- [7, 17]
     , pllPDIV <- [0, 2..31]
     , pllQ <- [2, 4, 6, 8]
     , pllR <- [2, 4, 6, 8]
-    , let pllIn = hsi / pllM
+    , let pllIn = fCin / pllM
        in pllIn >= 2.66e6 && pllIn <= 8e6    -- required range!
-    , let fVCO = hsi * pllN / pllM
+    , let fVCO = fCin * pllN / pllM
        in fVCO >= 64e6 && fVCO <= 344e6    -- allowed range!
        && fVCO / pllR <= 170e6
        && fVCO / pllQ <= 170e6

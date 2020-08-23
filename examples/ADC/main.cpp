2750150445fff2f3fe018399af36a5215f897357
Bengt Marten Agren <marangisto@me.com>
2020-08-23 13:25:10 +0800

    Added probe to ADC example.

--- examples/ADC/main.cpp
+++ examples/ADC/main.cpp
@@ -5,6 +5,7 @@
 
 using serial = board::serial;
 using led = board::led1;
+using probe = board::probe;
 using adc = adc_t<1>;
 
 template<> void handler<board::serial_interrupt>()
@@ -12,9 +13,16 @@ template<> void handler<board::serial_interrupt>()
     serial::isr();
 }
 
+template<> void handler<interrupt::ADC1_2>()
+{
+    probe::clear();
+    adc::clear_interrupt_flags(adc::EOS);
+}
+
 int main()
 {
     led::setup();
+    probe::setup();
     serial::setup<230400>();
     interrupt::set<board::serial_interrupt>();
     interrupt::enable();
@@ -23,9 +31,12 @@ int main()
 
     adc::setup();
     adc::enable();
+    adc::enable_interrupt(adc::EOS);
+    interrupt::set<interrupt::ADC1_2>();
 
     for (;;)
     {
+        probe::set();
         uint16_t x0 = adc::read<board::A0>();
         uint16_t x1 = adc::read<board::A1>();
 

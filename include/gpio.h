06ae83707dd4a4ba486171ea3b5e1c93cab48696
Bengt Marten Agren <marangisto@me.com>
2020-09-20 20:33:10 +0800

    First phase of refactoring for gpio driver.

--- include/gpio.h
+++ include/gpio.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "corona.h"
-#include <driver/gpio.h>
+#include <device/gpio.h>
 
 enum output_type_t
     { push_pull
@@ -48,20 +48,16 @@ static constexpr port_t pin_port()
 template<port_t PORT>
 using gpio_t = typename gpio_traits<PORT>::gpio;
 
+#include <driver/gpio.h>
+
 template<pin_t PIN>
-class output_t
+struct gpio_driver
 {
-public:
-    template
-        < output_type_t output_type = push_pull
-        , output_speed_t speed = low_speed
-        >
+    template<output_type_t output_type, output_speed_t speed>
     static inline void setup()
     {
         typename gpio_t<PORT>::T& GPIO = gpio_t<PORT>::V;
 
-        gpio_traits<PORT>::template enable<rcc_t>();
-        // FIXME: feature for STM32F1 family!
         GPIO.MODER &= ~(0x3 << (POS*2));
         GPIO.MODER |= output_mode << (POS*2);
         if (speed != low_speed)
@@ -70,19 +66,86 @@ public:
             GPIO.OTYPER |= MASK;
     }
 
+    template<input_type_t input_type>
+    static inline void setup()
+    {
+        typename gpio_t<PORT>::T& GPIO = gpio_t<PORT>::V;
+
+        GPIO.MODER &= ~(0x3 << (POS*2));    // N.B. input_mode is 0
+        if (input_type != floating)
+            GPIO.PUPDR |= input_type << (POS*2);
+    }
+
+    template
+        < signal_t SIG
+        , output_speed_t speed
+        , output_type_t output_type
+        >  // FIXME: should we not have output_type option here?
+    static inline void setup()
+    {
+        typename gpio_t<PORT>::T& GPIO = gpio_t<PORT>::V;
+
+        GPIO.MODER &= ~(0x3 << (POS*2));
+        GPIO.MODER |= alternate_mode << (POS*2);
+        if (speed != low_speed)
+            GPIO.OSPEEDR |= speed << (POS*2);
+        if (output_type == open_drain)
+            GPIO.OTYPER |= MASK;
+        if (POS < 8)
+            GPIO.AFRL |= signal_traits<gpio_conf, PIN, SIG>::AF << (POS*4);
+        else
+            GPIO.AFRH |= signal_traits<gpio_conf, PIN, SIG>::AF << ((POS-8)*4);
+    }
+
+    template<signal_t SIG, input_type_t input_type>
+    static inline void setup()
+    {
+        typename gpio_t<PORT>::T& GPIO = gpio_t<PORT>::V;
+
+        GPIO.MODER &= ~(0x3 << (POS*2));
+        GPIO.MODER |= alternate_mode << (POS*2);
+        if (input_type != floating)
+            GPIO.PUPDR |= input_type << (POS*2);
+        if (POS < 8)
+            GPIO.AFRL |= signal_traits<gpio_conf, PIN, SIG>::AF << (POS*4);
+        else
+            GPIO.AFRH |= signal_traits<gpio_conf, PIN, SIG>::AF << ((POS-8)*4);
+    }
+
+    static constexpr port_t PORT = pin_port<PIN>();
+    static constexpr uint8_t POS = pin_bit<PIN>();
+    static constexpr uint32_t MASK = 1 << POS;
+};
+
+template<pin_t PIN>
+class output_t: private gpio_driver<PIN>
+{
+public:
+    using BASE = gpio_driver<PIN>;
+
+    template
+        < output_type_t output_type = push_pull
+        , output_speed_t speed = low_speed
+        >
+    static inline void setup()
+    {
+        gpio_traits<BASE::PORT>::template enable<rcc_t>();
+        gpio_driver<PIN>::template setup<output_type, speed>();
+    }
+
     static inline void set()
     {
-        gpio_t<PORT>::V.BSRR = MASK;
+        gpio_t<BASE::PORT>::V.BSRR = BASE::MASK;
     }
 
     static inline void clear()
     {
-        gpio_t<PORT>::V.BSRR = MASK << 16;
+        gpio_t<BASE::PORT>::V.BSRR = BASE::MASK << 16;
     }
 
     static inline bool read()
     {
-        return (gpio_t<PORT>::V.ODR & MASK) != 0;
+        return (gpio_t<BASE::PORT>::V.ODR & BASE::MASK) != 0;
     }
 
     static inline bool write(bool x)
@@ -94,36 +157,30 @@ public:
     {
         write(!read());
     }
-
-private:
-    static constexpr port_t PORT = pin_port<PIN>();
-    static constexpr uint8_t POS = pin_bit<PIN>();
-    static constexpr uint32_t MASK = 1 << POS;
 };
 
 template<pin_t PIN>
-class input_t
+class input_t: private gpio_driver<PIN>
 {
 public:
+    using BASE = gpio_driver<PIN>;
+
     template<input_type_t input_type = floating>
     static inline void setup()
     {
-        typename gpio_t<PORT>::T& GPIO = gpio_t<PORT>::V;
-
-        gpio_traits<PORT>::template enable<rcc_t>();
-
-        // FIXME: refactor and feature for F1 series
-        GPIO.MODER &= ~(0x3 << (POS*2));    // N.B. input_mode is 0
-        if (input_type != floating)
-            GPIO.PUPDR |= input_type << (POS*2);
+        gpio_traits<BASE::PORT>::template enable<rcc_t>();
+        gpio_driver<PIN>::template setup<input_type>();
     }
 
-    static inline bool read() { return gpio_t<PORT>::V.IDR & MASK; }
+    static inline bool read()
+    {
+        return gpio_t<BASE::PORT>::V.IDR & BASE::MASK;
+    }
 
     template<trigger_edge_t EDGE = rising_edge>
     static void enable_interrupt()
     {
-        exti_interrupt<PORT, POS>::template enable
+        exti_interrupt<BASE::PORT, BASE::POS>::template enable
             < EDGE == rising_edge || EDGE == both_edges
             , EDGE == falling_edge || EDGE == both_edges
             >();
@@ -131,18 +188,13 @@ public:
 
     static inline bool interrupt_pending()
     {
-        return exti_interrupt<PORT, POS>::pending();
+        return exti_interrupt<BASE::PORT, BASE::POS>::pending();
     }
 
     static inline void clear_interrupt()
     {
-        exti_interrupt<PORT, POS>::clear();
+        exti_interrupt<BASE::PORT, BASE::POS>::clear();
     }
-
-private:
-    static constexpr port_t PORT = pin_port<PIN>();
-    static constexpr uint8_t POS = pin_bit<PIN>();
-    static constexpr uint32_t MASK = 1 << POS;
 };
 
 /*
@@ -166,50 +218,27 @@ private:
  */
 
 template<pin_t PIN, signal_t ALT>
-class alternate_t
+class alternate_t: private gpio_driver<PIN>
 {
 public:
+    using BASE = gpio_driver<PIN>;
+
     template
         < output_speed_t speed = low_speed
         , output_type_t output_type = push_pull
         >  // FIXME: should we not have output_type option here?
     static inline void setup()
     {
-        typename gpio_t<PORT>::T& GPIO = gpio_t<PORT>::V;
-
-        gpio_traits<PORT>::template enable<rcc_t>();
-        GPIO.MODER &= ~(0x3 << (POS*2));
-        GPIO.MODER |= alternate_mode << (POS*2);
-        if (speed != low_speed)
-            GPIO.OSPEEDR |= speed << (POS*2);
-        if (output_type == open_drain)
-            GPIO.OTYPER |= MASK;
-        if (POS < 8)
-            GPIO.AFRL |= signal_traits<gpio_conf, PIN, ALT>::AF << (POS*4);
-        else
-            GPIO.AFRH |= signal_traits<gpio_conf, PIN, ALT>::AF << ((POS-8)*4);
+        gpio_traits<BASE::PORT>::template enable<rcc_t>();
+        BASE::template setup<ALT, speed, output_type>();
     }
 
     template<input_type_t input_type = floating>
     static inline void setup()
     {
-        typename gpio_t<PORT>::T& GPIO = gpio_t<PORT>::V;
-
-        gpio_traits<PORT>::template enable<rcc_t>();
-        GPIO.MODER &= ~(0x3 << (POS*2));
-        GPIO.MODER |= alternate_mode << (POS*2);
-        if (input_type != floating)
-            GPIO.PUPDR |= input_type << (POS*2);
-        if (POS < 8)
-            GPIO.AFRL |= signal_traits<gpio_conf, PIN, ALT>::AF << (POS*4);
-        else
-            GPIO.AFRH |= signal_traits<gpio_conf, PIN, ALT>::AF << ((POS-8)*4);
+        gpio_traits<BASE::PORT>::template enable<rcc_t>();
+        BASE::template setup<ALT, input_type>();
     }
-
-private:
-    static constexpr port_t PORT = pin_port<PIN>();
-    static constexpr uint8_t POS = pin_bit<PIN>();
-    static constexpr uint32_t MASK = 1 << POS;
 };
 
 template<signal_t ALT>

1f3a6eff205fd37a06e8599bc9fbfe1f8c7321a0
Bengt Marten Agren <marangisto@me.com>
2020-09-25 09:42:27 +0800

    Share usart driver with G4.

--- include/stm32/stm32l4/driver/usart.h
+++ include/stm32/stm32l4/driver/usart.h
@@ -1,11 +1,1 @@
-#pragma once
-
-template<typename T>
-struct usart_driver
-{
-    static inline void write(uint8_t x) { T::V.TDR = x; }
-    static inline uint8_t read() { return T::V.RDR; }
-    static inline bool txe() { return T::V.ISR & T::T::ISR_TXE; }
-    static inline bool rxne() { return T::V.ISR & T::T::ISR_RXNE; }
-};
-
+#include "../../stm32g4/driver/usart.h"

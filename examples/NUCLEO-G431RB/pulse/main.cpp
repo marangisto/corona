eaa368cfa840d687786ee5b3f11eed0bdf9ecec7
Bengt Marten Agren <marangisto@me.com>
2021-01-03 08:07:02 +0800

    Minor adjustments on pulse example.

--- examples/NUCLEO-G431RB/pulse/main.cpp
+++ examples/NUCLEO-G431RB/pulse/main.cpp
@@ -10,17 +10,17 @@ using slave = tim_t<4>;
 template<> void handler<interrupt::TIM3>()
 {
     master::clear_update_interrupt_flag();
-    out::set();
     slave::set_count(0);
+    out::set();
     slave::clear_update_interrupt_flag();
     slave::enable_update_interrupt();
 }
 
 template<> void handler<interrupt::TIM4>()
 {
+    out::clear();
     slave::clear_update_interrupt_flag();
     slave::disable_update_interrupt();
-    out::clear();
 }
 
 int main()
@@ -31,14 +31,14 @@ int main()
     // f_tim = f_clock / ((psc + 1) (arr + 1))
     // arr = t_us * f_clock / (1e6 * (p + 1) - 1
 
-    const auto t1 = 100;    // period in micro-seconds
+    const auto t1 = 10;     // period in micro-seconds
     const auto pre1 = 0;    // prescaler
     const auto arr1 = t1 * (master::clock() / 1000000) / (pre1 + 1) - 1;
     master::setup(pre1, arr1);
     master::enable_update_interrupt();
     interrupt::set<interrupt::TIM3>();
 
-    const auto t2 = 10;     // period in micro-seconds
+    const auto t2 = 1;      // period in micro-seconds
     const auto pre2 = 0;    // prescaler
     const auto arr2 = t2 * (master::clock() / 1000000) / (pre2 + 1) - 1;
     slave::setup(pre2, arr2);

34f599275b0fb26b6d4e08e93da7eda6558e3fe2
Bengt Marten Agren <marangisto@me.com>
2021-06-12 16:14:55 +0800

    Serial and driver skeleton.

--- examples/NUCLEO-H743ZI2/fdcan/main.cpp
+++ examples/NUCLEO-H743ZI2/fdcan/main.cpp
@@ -1,11 +1,42 @@
 #include "../board.h"
 #include <timer.h>
+#include <cstring>
+#include <textio.h>
+#include <usart.h>
+#include <device/fdcan.h>
 
+using serial = usart_t<SERIAL_USART, SERIAL_TX, SERIAL_RX>;
 using led = output_t<LED>;
 using out = output_t<PROBE>;
 using tim = tim_t<TIMER_NO>;
 using tx = output_t<PB9>;
 using rx = input_t<PB8>;
+
+template<int INST>
+struct fd_can_t
+{
+    static void setup()
+    {
+        fdcan_traits<0>::template enable<rcc_t>();   // enable clock
+    }
+
+    static uint32_t reg()
+    {
+        return fdcan::V.FDCAN_NBTP;
+    }
+
+private:
+    using traits = fdcan_traits<INST>;
+    using fdcan = typename traits::fdcan;
+    using _ = typename fdcan::T;
+};
+
+using fdcan = fd_can_t<1>;
+
+template<> void handler<SERIAL_ISR>()
+{
+    serial::isr();
+}
 
 template<> void handler<TIMER_ISR>()
 {
@@ -30,8 +61,17 @@ int main()
     interrupt::set<TIMER_ISR>();
     interrupt::enable();
 
+    serial::setup<230400>();
+    interrupt::set<SERIAL_ISR>();
+    interrupt::enable();
+
+    printf<serial>("Hello World!\n");
+    printf<serial>("sys-clock = %d\n", sys_clock::freq());
+
     tx::setup();
     rx::setup();
+    fdcan::setup();
+    printf<serial>("sys-clock = %04x\n", fdcan::reg());
 
     for (;;)
     {
